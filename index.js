1cf0a259f3e442fcc12d99549348051df92ba6ca
Dylan <34420038+DPM97@users.noreply.github.com>
2020-06-02 17:45:37 -0400

    f

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const color = require('colorthief');
 const request = require('request');
 const config = require('./config');
-const ig = require('node-instagram');
 const nc = require('node-cache');
 const Cache = new nc();
 const fs = require('fs');
+const Instagram = require('instagram-web-api')
+const user = config.instagram.username.toString()
+const pass = config.instagram.password.toString()
+const IGClient = new Instagram({username: user, password: pass})
+let queue = [];
 let proxies = [];
 
+
 const main = async () => {
+    startQueue();
+    await IGClient.login({username: user, password: pass})
     await fs.readFile('proxies.txt', 'utf-8', (err, out) => { proxies = out.split('\r\n') })
     await getRecent();
     let f = setInterval(async () => {
@@ -42,7 +49,10 @@ const handleRecent = async (recent) => {
             delete recent[key]
         } else {
             await Cache.set(key, recent[key], config.cacheTTL);
-            send(key);
+            comment(key);
+            /**
+             * send(key) if you would like to send the post shortCode to a server to process it.
+             */
         }
     }
 }
@@ -56,6 +66,12 @@ const send = async (id) => {
         },
         json: true,
     }, (err, res, body) => { if (err) console.log(err) })
+}
+
+const comment = async (id) => {
+    const media = await IGClient.getMediaByShortcode({ shortcode: id })
+    console.log(`https://instagram.com/p/${id}`)
+    queue.push(media.id)
 }
 
 const getAvgColor = async (url) => {
@@ -89,6 +105,14 @@ const proxyLogic = async (url) => {
           resolve(`http://${p[2]}:${p[3]}@${p[0]}:${p[1]}`)
       }
     })
+}
+
+const startQueue = async () => {
+    let f = setInterval(async () => {
+        if (queue.length > 0) {
+            await IGClient.addComment({ mediaId: queue.shift(), text: config.instagram.message.toString() })
+        }
+    }, 15000)
 }
 
 
